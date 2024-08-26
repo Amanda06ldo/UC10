bfeb3f91ac02275ef8c2e4975e9a3334fcc9052b
Amanda06ldo <162368771+Amanda06ldo@users.noreply.github.com>
2024-08-26 16:58:50 -0300

    Add files via upload

---
+++ branch.ts
@@ -0,0 +1,66 @@
+var reader = require("readline-sync")
+
+class branch {
+    public nome: string
+    public dataCriacao:Date
+    public commits: Array<string> = []
+    public branchPai:branch
+
+    constructor(nome:string, dataCriacao:Date){
+        this.nome = nome
+        this.dataCriacao = dataCriacao
+    }
+
+    public criarCommit():void{
+    let adicionar = reader.question("escreva o que quer adicionar no commit: ")
+    this.commits.push(adicionar)
+    }
+
+    public merge(outraBranch:branch):void{
+    this.commits.concat(outraBranch.commits)
+    }
+
+    public checkout():void{
+    let escolher = reader.questionInt('Qual branch voce quer escolher?')
+
+    return escolher
+    }
+
+    public mostrarHistorico():void{
+    console.log(this.commits)
+    }
+}
+
+class branchMaster extends branch {
+    criarNovaBranch(nome:string,dataCriacao:Date):branch{
+        let newBranch = new branch(nome,dataCriacao)
+        return newBranch
+    }
+} 
+
+class branchDevelop extends branch{
+    finalizarSprint(branchRelease:branchRelease):void{
+    this.merge(branchRelease)
+    }
+}
+
+class branchHotfix extends branch{
+    corrigirBug(descricao:string):void{
+    this.commits.push(descricao)
+    }
+}
+
+class branchFeature extends branch{
+    implementarFuncionalidade(descricao:string):void{
+    this.commits.push(descricao)
+    }
+}
+
+class branchRelease extends branch{
+    promoverParaProducao():void{
+        console.log(`
+        A branch nova ficou assim: 
+        ${this.commits}
+        A branch está pronta pra ser lancada!!`)
+    }
+}
